8143f71417528cc15e300d7a657535ae3c53eb9f
ignatown <ignatow.n@mail.ru>
2022-01-04 04:08:40 +0300

    2. Spring boot Webflux [#328882]

---
+++ src/main/java/ru/job4j/weather/WeatherService.java
@@ -0,0 +1,43 @@
+package ru.job4j.weather;
+
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.Comparator;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
+
+@Service
+public class WeatherService {
+
+    private final Map<Integer, Weather> weathers = new ConcurrentHashMap<>();
+
+    {
+        weathers.put(1, new Weather(1, "Msc", 20));
+        weathers.put(2, new Weather(2, "SPb", 15));
+        weathers.put(3, new Weather(3, "Bryansk", 15));
+        weathers.put(4, new Weather(4, "Smolensk", 15));
+        weathers.put(5, new Weather(5, "Kiev", 15));
+        weathers.put(6, new Weather(6, "Minsk", 15));
+        weathers.put(7, new Weather(7, "Ryazan", 25));
+    }
+
+    public Mono<Weather> findById(Integer id) {
+        return Mono.justOrEmpty(weathers.get(id));
+    }
+
+    public Flux<Weather> all() {
+        return Flux.fromIterable(weathers.values());
+    }
+
+    public Mono<Weather> hottest() {
+        return Mono.justOrEmpty(weathers.values().stream().max(Comparator.comparing(Weather::getTemperature)));
+    }
+
+    public Flux<Weather> greatThen(int temp) {
+        return Flux.fromIterable(weathers.values().stream().filter(c -> c.getTemperature() > temp)
+                .collect(Collectors.toList()));
+    }
+}
